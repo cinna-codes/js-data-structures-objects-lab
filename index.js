ebd1bc7b4b2d4bd974012b5abbcc58a5e1073673
cinna-codes <somethingfellipickeditup@gmail.com>
2021-02-15 18:36:29 -0500

    commit tree 43bcef159975a2873c9d2d962af1e96dabbe4733

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newDriver = {...driver}
+    newDriver[key] = value
+    return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+    // deletes `key` from a clone of driver and returns the new driver (it is non-destructive):
+    // does not modify the original driver (it is non-destructive):
+    const newDriver = {...driver}
+    delete newDriver[key]
+    return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    // returns driver without the delete key/value pair:
+    // modifies the original driver:
+    delete driver[key]
+    return driver
+}
+
